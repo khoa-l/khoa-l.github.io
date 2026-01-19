e28cc7e71eaf4cc6c760eef1d0b2b7aeaf880571
khoa-l <kly27@calpoly.edu>
2026-01-19 03:52:46 -0800

    feat: configure for root deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/www",
+  // basePath: "/www",
   images: {
     unoptimized: true,
   },
