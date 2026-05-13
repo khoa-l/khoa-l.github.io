62b7ff8629b300e91fb420d30a99f3dbe22592a7
khoa-l <kly27@calpoly.edu>
2026-05-13 13:00:21 -0700

    feat: different type declaration for svgs

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,5 +1,10 @@
 declare module "*.svg" {
-  import type { FC, SVGProps } from "react";
-  const ReactComponent: FC<SVGProps<SVGSVGElement>>;
+  import * as React from "react";
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
+}
+
+declare module "*.svg?url" {
+  const content: string;
+  export default content;
 }
