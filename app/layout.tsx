60121d71305c19d80e7de1c0759120c3173bb465
khoa-l <kly27@calpoly.edu>
2026-08-22 03:49:44 -0700

    feat: setup portals for base ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${inclusiveSans.variable} ${paperMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div className="root">{children}</div>
+      </body>
     </html>
   );
 }
